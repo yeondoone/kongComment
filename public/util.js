2dc3ba9d43a32cd0be8f2f04f88f22344c4b4ef8
yeondoone <yeondoone@gmail.com>
2016-09-23 00:35:42 +0900

    링크 글쓰기
    포스팅시 앱정보가 삽입된다.
    삽입되는 앱 아이콘과 이름은 카카오디벨로퍼 앱 일반설정에서 수정 가능하다.

--- public/util.js
+++ public/util.js
@@ -41,6 +41,27 @@ function setCookie(cookieName, cookieValue, expireDate){
     var today = new Date();
     
     today.setDate(today.getDate() + parseInt(expireDate));
-    document.cookie = cookieName + "=" +escape(cookieValue) + "; path=/; expires=" + today.getGMTString() + ";";
+    document.cookie = cookieName + "=" +escape(cookieValue) + "; path=/; expires=" + today.toGMTString() + ";";
 }
 
+function getCookie(cookieName){
+    var search = cookieName + '=';
+    var cookie = document.cookie;
+    
+    if(cookie.length > 0){
+        startIndex = cookie.indexOf(cookieName);
+        
+        if(startIndex != -1){
+            startIndex += cookieName.length;
+            endIndex = cookie.indexOf(";", startIndex);
+            
+            if(endIndex == -1) endIndex=cookie.length;
+            
+            return unescape(cookie.substring(startIndex + 1, endIndex));
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+}
